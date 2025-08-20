3f093f6a052e554e42188f63c82147b4f7c7aa78
jcs-123 <jcs@jecc.ac.in>
2025-08-20 14:49:50 +0530

    backend

--- server.js
+++ server.js
@@ -26,8 +26,7 @@ app.use(cors());
 app.use(express.json());
 
 // Serve uploaded files
-app.use("/uploads", express.static("uploads"));
-
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // Routes
 app.use('/api/staff', staffRoutes);
